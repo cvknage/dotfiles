fcfd14a61445b355534bc601e06f311e04b8745f
Christophe Knage <cvknage@gmail.com>
2023-10-01 00:02:11 +0200

    Updated colorscheme

--- lazyvim/lua/plugins/colorschema.lua
+++ lazyvim/lua/plugins/colorschema.lua
@@ -2,7 +2,7 @@ return {
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin", -- <catppuccin, catppuccin-latte, catppuccin-frappe, catppuccin-macchiato, catppuccin-mocha>
+      colorscheme = "catppuccin-mocha", -- <catppuccin, catppuccin-latte, catppuccin-frappe, catppuccin-macchiato, catppuccin-mocha>
       -- colorscheme = "habamax",
       -- colorscheme = tokyonight",
     },
