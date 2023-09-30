956a4fa4abc76c712053dbf22e2fdbfd02bf6d6f
Christophe Knage <cvknage@gmail.com>
2023-09-30 23:34:27 +0200

    Don't let neotree have focus on startup

--- lazyvim/lua/plugins/neotree.lua
+++ lazyvim/lua/plugins/neotree.lua
@@ -7,4 +7,13 @@ return {
       },
     },
   },
+  init = function()
+    if vim.fn.argc() == 1 then
+      local stat = vim.loop.fs_stat(tostring(vim.fn.argv(0)))
+      if stat and stat.type == "directory" then
+        require("neo-tree")
+        vim.cmd("Neotree show")
+      end
+    end
+  end,
 }
