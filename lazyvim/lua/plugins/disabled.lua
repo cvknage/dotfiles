0e6098a1bb92aed4267d170be4dfa77e6517c2b5
Christophe Knage <cvknage@gmail.com>
2023-10-02 21:32:36 +0200

    Disabled tokyonight theme

--- lazyvim/lua/plugins/disabled.lua
+++ lazyvim/lua/plugins/disabled.lua
@@ -1,4 +1,6 @@
 return {
   { "rcarriga/nvim-notify", enabled = false },
   { "folke/noice.nvim", enabled = false },
+  { "folke/tokyonight.nvim ", enabled = false },
+  -- { "nvim-neo-tree/neo-tree.nvim", enabled = false },
 }
