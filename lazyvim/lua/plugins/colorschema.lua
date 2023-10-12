17256fb01b1e11f8110b9bf3e0d66509049e4238
Christophe Knage <cvknage@gmail.com>
2023-10-12 23:19:43 +0200

    Added background transparency

--- lazyvim/lua/plugins/colorschema.lua
+++ lazyvim/lua/plugins/colorschema.lua
@@ -2,9 +2,15 @@ return {
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin-macchiato", -- <catppuccin, catppuccin-latte, catppuccin-frappe, catppuccin-macchiato, catppuccin-mocha>
-      -- colorscheme = "habamax",
-      -- colorscheme = tokyonight",
+      colorscheme = "catppuccin-macchiato", -- <catppuccin-latte, catppuccin-frappe, catppuccin-macchiato, catppuccin-mocha>
+    },
+  },
+  {
+    "catppuccin/nvim",
+    lazy = true,
+    name = "catppuccin",
+    opts = {
+      transparent_background = true, -- disables setting the background color.
     },
   },
 }
