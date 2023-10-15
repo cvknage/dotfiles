ecb1d64c7f78b1348843310046ee1f95c94280d8
Christophe Knage <cvknage@gmail.com>
2023-10-15 22:42:30 +0200

    Re-Enable noice.nvim

--- lazyvim/lua/plugins/disabled.lua
+++ lazyvim/lua/plugins/disabled.lua
@@ -1,7 +1,8 @@
 return {
   { "rcarriga/nvim-notify", enabled = false },
-  { "folke/noice.nvim", enabled = false },
   { "folke/tokyonight.nvim ", enabled = false },
+  -- { "akinsho/bufferline.nvim", enabled = false },
+  -- { "folke/noice.nvim", enabled = false },
   -- { "f-person/auto-dark-mode.nvim", enabled = false },
   -- { "nvim-neo-tree/neo-tree.nvim", enabled = false },
 }
