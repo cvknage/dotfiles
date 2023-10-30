685aff41ab8895a71391a746b61d18ba380e849c
Christophe Knage <cvknage@gmail.com>
2023-10-30 21:58:28 +0100

    Added comments to describe options

--- custom/lua/default/config/options.lua
+++ custom/lua/default/config/options.lua
@@ -1,33 +1,31 @@
--- vim.opt.guicursor = ""
+vim.opt.number = true -- Print line number
+vim.opt.relativenumber = true -- Relative line numbers
 
-vim.opt.nu = true
-vim.opt.relativenumber = true
+vim.opt.tabstop = 4 -- Number of spaces tabs count for
+vim.opt.softtabstop = 4 -- Number of spaces tabs count for while performing editing operatons
+vim.opt.shiftwidth = 4 -- Size of an indent
+vim.opt.expandtab = true -- Use spaces instead of tabs
+vim.opt.smartindent = true -- Insert indents automatically
 
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
+vim.opt.wrap = false -- Disable line wrap
 
-vim.opt.smartindent = true
+vim.opt.swapfile = false -- Disable swap file - use undotree
+vim.opt.backup = false -- Disable backup files - use undotree
+vim.opt.undofile = true -- Enable undo file for undotree 
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir" -- Undo dir for undotree
 
-vim.opt.wrap = false
+vim.opt.hlsearch = true -- Enable search highlight
+vim.opt.incsearch = true -- Enable incremental search
 
-vim.opt.swapfile = false
-vim.opt.backup = false
-vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
-vim.opt.undofile = true
+vim.opt.termguicolors = true -- True color support
 
-vim.opt.hlsearch = true
-vim.opt.incsearch = true
-
-vim.opt.termguicolors = true
-
-vim.opt.scrolloff = 8
-vim.opt.signcolumn = "yes"
+vim.opt.scrolloff = 8 -- Lines of context
+vim.opt.signcolumn = "yes" -- Always show the signcolumn, otherwise it would shift the text each time
 vim.opt.isfname:append("@-@")
 
-vim.opt.updatetime = 50
+vim.opt.updatetime = 50 -- Save swap file and trigger CursorHold
 
-vim.opt.colorcolumn = "80"
+vim.opt.colorcolumn = "120" -- Code length guide
+
 vim.opt.pumblend = 10 -- Popup blend
 vim.opt.winblend = 10 -- Window blend
