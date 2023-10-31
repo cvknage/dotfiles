f1ff4bce43d153c53c5c8296fc732c5188f30cac
Christophe Knage <cvknage@gmail.com>
2023-10-31 23:34:30 +0100

    Added which-key plugin

--- custom/lua/default/plugins/colorscheme.lua
+++ custom/lua/default/plugins/colorscheme.lua
@@ -1,50 +1,51 @@
 return {
-	"catppuccin/nvim",
-	name = "catppuccin",
-	priority = 1000,
-	config = function(_, opts)
-        require('catppuccin').setup(opts)
-        vim.cmd.colorscheme("catppuccin")
-        vim.opt.pumblend = 10 -- Enables pseudo-transparency for the |popup-menu|
-        vim.opt.winblend = 10 -- Enables pseudo-transparency for a floaring window
-	end,
-	opts = {
-        flavour = "macchiato", -- latte, frappe, macchiato, mocha
-        background = { -- :h background
-            light = "frappe",
-            dark = "mocha",
+  "catppuccin/nvim",
+  name = "catppuccin",
+  priority = 1000,
+  config = function(_, opts)
+    require('catppuccin').setup(opts)
+    vim.cmd.colorscheme("catppuccin")
+    vim.opt.pumblend = 10     -- Enables pseudo-transparency for the |popup-menu|
+    vim.opt.winblend = 10     -- Enables pseudo-transparency for a floaring window
+  end,
+  opts = {
+    flavour = "macchiato",     -- latte, frappe, macchiato, mocha
+    background = {             -- :h background
+      light = "frappe",
+      dark = "mocha",
+    },
+    transparent_background = true,     -- disables setting the background color
+    custom_highlights = function(color)
+      return {
+        NormalFloat = { bg = color.base },                        -- background for floaring windows like :Mason and :Lazy
+        TreesitterContextLineNumber = { bg = color.base }         -- background for treesitter context line numbers
+      }
+    end,
+    integrations = {
+      treesitter = true,
+      mason = true,
+      telescope = {
+        enabled = true,
+      },
+      native_lsp = {
+        enabled = true,
+        virtual_text = {
+          errors = { "italic" },
+          hints = { "italic" },
+          warnings = { "italic" },
+          information = { "italic" },
         },
-		transparent_background = true, -- disables setting the background color
-        custom_highlights = function(color)
-          return {
-            NormalFloat = { bg = color.base }, -- background for floaring windows like :Mason and :Lazy
-            TreesitterContextLineNumber = { bg = color.base } -- background for treesitter context line numbers
-          }
-        end,
-        integrations = {
-            treesitter = true,
-            mason = true,
-            telescope = {
-                enabled = true,
-            },
-            native_lsp = {
-                enabled = true,
-                virtual_text = {
-                    errors = { "italic" },
-                    hints = { "italic" },
-                    warnings = { "italic" },
-                    information = { "italic" },
-                },
-                underlines = {
-                    errors = { "underline" },
-                    hints = { "underline" },
-                    warnings = { "underline" },
-                    information = { "underline" },
-                },
-                inlay_hints = {
-                    background = true,
-                },
-            },
+        underlines = {
+          errors = { "underline" },
+          hints = { "underline" },
+          warnings = { "underline" },
+          information = { "underline" },
         },
-	},
+        inlay_hints = {
+          background = true,
+        },
+      },
+      which_key = true
+    },
+  },
 }
---
+++ custom/lua/default/plugins/which-key.lua
@@ -0,0 +1,39 @@
+return {
+  "folke/which-key.nvim",
+  event = "VeryLazy",
+  init = function()
+    vim.o.timeout = true
+    vim.o.timeoutlen = 500
+  end,
+  opts = {
+    -- your configuration comes here or leave it empty to use the default settings
+    -- refer to the configuration section: https://github.com/folke/which-key.nvim#%EF%B8%8F-configuration
+
+    --plugins = { spelling = true },
+    defaults = {
+      mode = { "n", "v" },
+      ["g"] = { name = "+goto" },
+      -- ["gs"] = { name = "+surround" },
+      ["]"] = { name = "+next" },
+      ["["] = { name = "+prev" },
+      ["<leader><tab>"] = { name = "+tabs" },
+      ["<leader>b"] = { name = "+buffer" },
+      ["<leader>c"] = { name = "+code" },
+      ["<leader>d"] = { name = "+dap" },
+      ["<leader>f"] = { name = "+file/find" },
+      -- ["<leader>g"] = { name = "+git" },
+      -- ["<leader>gh"] = { name = "+hunks" },
+      ["<leader>p"] = { name = "+project" },
+      -- ["<leader>q"] = { name = "+quit/session" },
+      ["<leader>s"] = { name = "+search" },
+      ["<leader>u"] = { name = "+ui" },
+      -- ["<leader>w"] = { name = "+windows" },
+      -- ["<leader>x"] = { name = "+diagnostics/quickfix" },
+    },
+  },
+  config = function(_, opts)
+    local wk = require("which-key")
+    wk.setup(opts)
+    wk.register(opts.defaults)
+  end,
+}
