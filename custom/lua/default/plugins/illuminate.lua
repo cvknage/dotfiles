0099fcf2c0e2f741de2ad4e0fc5453a0a659a31e
Christophe Knage <cvknage@gmail.com>
2023-11-13 21:37:19 +0100

    added vim-illuminate

---
+++ custom/lua/default/plugins/illuminate.lua
@@ -0,0 +1,19 @@
+-- https://github.com/RRethy/vim-illuminate
+return {
+  "RRethy/vim-illuminate",
+  event = "VeryLazy",
+  opts = {
+    -- large_file_cutoff: number of lines at which to use large_file_config
+    -- The `under_cursor` option is disabled when this cutoff is hit
+    large_file_cutoff = 2000,
+    -- large_file_config: config to use for large files (based on large_file_cutoff).
+    -- Supports the same keys passed to .configure
+    -- If nil, vim-illuminate will be disabled for large files.
+    large_file_overrides = {
+      providers = { "lsp" },
+    },
+  },
+  config = function (_, opts)
+    require("illuminate").configure(opts)
+  end
+}
