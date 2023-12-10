f01b8642aaa396de0cad7dd182c9064ea259a2d3
Christophe Knage <cvknage@gmail.com>
2023-12-10 22:32:55 +0100

    added reference to sessionizer script

--- logic/lua/config/keymaps.lua
+++ logic/lua/config/keymaps.lua
@@ -15,7 +15,7 @@ vim.keymap.set({"n", "v" }, "<leader>cs", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Lef
 vim.keymap.set("n", "<leader>cd", vim.diagnostic.open_float, { desc = "Line Diagnostics" }) -- display line diagnostic
 
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, { desc = "Project volumes" }) -- Project Volumes: opens netrw
--- vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>") -- switch project
+-- vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>") -- switch project - https://github.com/edr3x/tmux-sessionizer
 
 vim.keymap.set({ "i", "n" }, "<esc>", "<cmd>noh<cr><esc>", { desc = "Escape and clear hlsearch" }) -- Clear search with <esc>
 
