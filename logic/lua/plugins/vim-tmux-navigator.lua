dd6394d41cad8e388bb78262686c7704bffbb08a
Christophe Knage <cvknage@gmail.com>
2023-12-11 19:36:38 +0100

    lazy load vim-tmux-navigator if tmux is installed

--- logic/lua/plugins/vim-tmux-navigator.lua
+++ logic/lua/plugins/vim-tmux-navigator.lua
@@ -1,3 +1,17 @@
-return {
+local M = {}
+
+M.navigator = {
   "christoomey/vim-tmux-navigator",
+  keys = {
+    { "<C-h>", desc = "Go to left window" },
+    { "<C-j>", desc = "Go to lower window" },
+    { "<C-k>", desc = "Go to upper window" },
+    { "<C-l>", desc = "Go to right window" },
+  },
 }
+
+if vim.fn.system({ "which", "tmux" }) ~= "" then
+  return M.navigator
+end
+
+return {}
