0cb875f4d2e3b72d8140494a8eea40581c1d85b3
Christophe Knage <cvknage@gmail.com>
2024-01-25 23:13:54 +0100

    added some VSCode keymaps to mimic my ordinary Neovim functionality

--- logic/init.lua
+++ logic/init.lua
@@ -3,6 +3,12 @@ if vim.g.vscode then
   require("config.autocmds")
   require("config.keymaps")
   require("config.options")
+
+  -- https://github.com/vscode-neovim/vscode-neovim#%EF%B8%8F-keybindings-shortcuts
+  vim.keymap.set("n", "<leader><space>", "<cmd>Find<cr>")
+  vim.keymap.set("n", "<leader>ff", "<cmd>Find<cr>")
+  vim.keymap.set("n", "<leader>/", function() require('vscode-neovim').call('workbench.action.findInFiles') end)
+  vim.keymap.set("n", "<leader>cf", function() require('vscode-neovim').call('editor.action.formatDocument') end)
 else
   -- ordinary Neovim
   require("config.autocmds")
