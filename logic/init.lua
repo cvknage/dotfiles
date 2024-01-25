14a1a32066f84a1700f0b2241961ef07314d98cc
Christophe Knage <cvknage@gmail.com>
2024-01-25 21:13:23 +0100

    added  simple config for Neovim VSCode extension

--- logic/init.lua
+++ logic/init.lua
@@ -1,4 +1,11 @@
-require("config.autocmds")
-require("config.keymaps")
-require("config.lazy")
-require("config.options")
+if vim.g.vscode then
+  -- VSCode extension
+  require("config.keymaps")
+  require("config.options")
+else
+  -- ordinary Neovim
+  require("config.autocmds")
+  require("config.keymaps")
+  require("config.lazy")
+  require("config.options")
+end
