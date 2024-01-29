2f30b318ade3f1de24452f1222ac4255ae17652d
Christophe Knage <cvknage@gmail.com>
2024-01-29 19:23:54 +0100

    disable Tabby on non Darwin machines

--- logic/lua/plugins/ai/tabby.lua
+++ logic/lua/plugins/ai/tabby.lua
@@ -15,11 +15,13 @@ return {
       pcall(io.popen, "tabby serve --device metal --model TabbyML/StarCoder-3B > /dev/null 2>&1 &")
       -- pcall(io.popen, "tabby serve --device metal --model TabbyML/CodeLlama-7B > /dev/null 2>&1 &")
     else
+      --[[
       if vim.fn.executable("docker") ~= 1 then
         pcall(io.popen, "docker run -it -p 8080:8080 -v $HOME/.tabby:/data tabbyml/tabby serve --model TabbyML/StarCoder-3B > /dev/null 2>&1 &")
         -- pcall(io.popen, "docker run -it --gpus all -p 8080:8080 -v $HOME/.tabby:/data tabbyml/tabby serve --model TabbyML/StarCoder-3B --device cuda > /dev/null 2>&1 &")
       end
-          end
+      --]]
+    end
   end,
   config = function()
     vim.g.tabby_keybinding_accept = "<C-y>"
