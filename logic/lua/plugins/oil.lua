46e907b091619805df7026973c1222d418dbb2b8
Christophe Knage <cvknage@gmail.com>
2024-05-10 19:07:31 +0200

    open oil instead of netrw then starting nvim from a doretory - but keep netrw installed and available

--- logic/lua/plugins/oil.lua
+++ logic/lua/plugins/oil.lua
@@ -1,7 +1,10 @@
+vim.cmd('autocmd VimEnter * ++once silent! autocmd! FileExplorer *')
+
 return {
   {
     'stevearc/oil.nvim',
     dependencies = { "nvim-tree/nvim-web-devicons" }, -- optional
+    lazy = false,
     keys = {
       { "<leader>pv", "<cmd>Oil<cr>", desc = "Project Volumes" },
     },
@@ -15,5 +18,16 @@ return {
         show_hidden = true,
       }
     },
+    config = function(_, opts)
+      require("oil").setup(opts)
+
+      local args = vim.v.argv
+      for _, arg in ipairs(args) do
+        if vim.fn.isdirectory(arg) == 1 then
+          vim.cmd('Oil')
+          break
+        end
+      end
+    end
   }
 }
