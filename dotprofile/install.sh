815adcec9a71aff2ab3fc10b1ed99dd361fa392c
Christophe Knage <cvknage@gmail.com>
2024-09-30 21:13:08 +0200

    variabl consistency

--- dotprofile/install.sh
+++ dotprofile/install.sh
@@ -1,29 +1,31 @@
 #!/usr/bin/env bash
 
-DOTPROFILE_FILE="$(pwd)/dotprofile"
+CUSTOM_DOTFILE="$(pwd)/dotprofile"
+DOT_PROFILE="${HOME}/.profile"
+DOT_ZPROFILE="${HOME}/.zprofile"
 
-file=""
-if [ -n "$BASH_VERSION" ]; then
-  file="${HOME}/.profile"
-elif [ -n "$ZSH_VERSION" ]; then
-  file="${HOME}/.zprofile"
+USER_DOTPROFILE=""
+if [ -f "$DOT_ZPROFILE" ]; then
+  USER_DOTPROFILE="$DOT_ZPROFILE"
+elif [ -f "$DOT_PROFILE" ]; then
+  USER_DOTPROFILE="$DOT_PROFILE"
 fi
 
-if [ -f "$file" ]; then
-  source_file="DOTPROFILE_FILE=${DOTPROFILE_FILE}"
-  comment_message='# load custom .(z)profile'
-  comment_warning='(keep comment for scripted removal)'
-  start_comment="${comment_message} - START - ${comment_warning}"
-  load_dotrc='
-if [ -f "$DOTPROFILE_FILE" ]; then
-  . "$DOTPROFILE_FILE"
+if [ -f "$USER_DOTPROFILE" ]; then
+  SOURCE_FILE="CUSTOM_DOTFILE=${CUSTOM_DOTFILE}"
+  COMMENT_MESSAGE='# load custom .(z)profile'
+  COMMENT_WARNING='(keep comment for scripted removal)'
+  START_COMMENT="${COMMENT_MESSAGE} - START - ${COMMENT_WARNING}"
+  LOAD_DOTFILE='
+if [ -f "$CUSTOM_DOTFILE" ]; then
+  . "$CUSTOM_DOTFILE"
 fi'
-  end_comment="${comment_message} - END - ${comment_warning}"
+  END_COMMENT="${COMMENT_MESSAGE} - END - ${COMMENT_WARNING}"
 
-  if ! grep -qF -- "$source_file" "$file"; then
-    sed -i"" -e "/$start_comment/,/$end_comment/d" $file
-    echo -e "$start_comment\n$source_file $load_dotrc\n$end_comment\n" >>"$file"
-    . $file
+  if ! grep -qF -- "$SOURCE_FILE" "$USER_DOTPROFILE"; then
+    sed -i"" -e "/$START_COMMENT/,/$END_COMMENT/d" $USER_DOTPROFILE
+    echo -e "$START_COMMENT\n$SOURCE_FILE $LOAD_DOTFILE\n$END_COMMENT\n" >>"$USER_DOTPROFILE"
+    . $USER_DOTPROFILE
   fi
 fi
 
