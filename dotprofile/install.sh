d9ae9d9eb5ca3369f911f478fab415d508555978
Christophe Knage <cvknage@gmail.com>
2024-09-30 20:39:12 +0200

    fixed dotprofile install script

--- dotprofile/install.sh
+++ dotprofile/install.sh
@@ -9,7 +9,7 @@ elif [ -n "$ZSH_VERSION" ]; then
   file="${HOME}/.zprofile"
 fi
 
-if [ -n "$file" ]; then
+if [ -f "$file" ]; then
   source_file="DOTPROFILE_FILE=${DOTPROFILE_FILE}"
   comment_message='# load custom .(z)profile'
   comment_warning='(keep comment for scripted removal)'
@@ -21,7 +21,7 @@ fi'
   end_comment="${comment_message} - END - ${comment_warning}"
 
   if ! grep -qF -- "$source_file" "$file"; then
-    sed -i "/$start_comment/,/$end_comment/d" $file
+    sed -i"" -e "/$start_comment/,/$end_comment/d" $file
     echo -e "$start_comment\n$source_file $load_dotrc\n$end_comment\n" >>"$file"
     . $file
   fi
