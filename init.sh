d1cd17a46322fefdaf6751f76df907408e10cb37
Christophe Knage <cvknage@gmail.com>
2024-12-30 00:32:03 +0100

    fixed first time init

--- init.sh
+++ init.sh
@@ -1,18 +1,29 @@
 #!/usr/bin/env bash
 
+set -e
+
 git config user.name "$(git log --reverse --format=%an | head -n 1)"
 git config user.email "$(git log --reverse --format=%ae | head -n 1)"
-
-if ! command -v nix > /dev/null; then
-  curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | \
-  sh -s -- install
-fi
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 DOTFILES_DIR="$HOME/.dotfiles"
 if [ -L $DOTFILES_DIR ] || [ ! -d $DOTFILES_DIR ]; then
   rm $DOTFILES_DIR &> /dev/null
   ln -s $SCRIPT_DIR $DOTFILES_DIR
+fi
+
+if ! command -v nix > /dev/null; then
+  curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | \
+  sh -s -- install
+
+  # The determinate systems nix installer finishes with the line below:
+  # To get started using Nix, open a new shell or run `. /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh`
+  echo "Please open a new shell, it works better..."
+  echo "Then, to continue initializing dotfiles, run \`bash ~/.dotfiles/init.sh\`"
+  echo ""
+
+  kill $(jobs -p) &> /dev/null
+  exit 0
 fi
 
 pushd $DOTFILES_DIR &> /dev/null
@@ -46,7 +57,9 @@ do
 done
 
 if ! command -v kanata > /dev/null; then
-  bash ./kanata/kanata_install.sh
+  pushd ./kanata &> /dev/null
+  bash ./kanata_install.sh
+  popd &> /dev/null
 fi
 
 popd &> /dev/null
