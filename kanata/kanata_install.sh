4a3c66e444b97137d2b7c9465b5e3cd94e2a0c5b
Christophe Knage <cvknage@gmail.com>
2025-01-20 21:01:21 +0100

    added guard against wrongly installing kanata on nixos

--- kanata/kanata_install.sh
+++ kanata/kanata_install.sh
@@ -5,21 +5,34 @@ KANATA_GIT_REPO="https://github.com/jtroo/kanata.git"
 KANATA_GIT_CHECKOUT="kanata.git"
 . ./kanata_variables.sh
 
-echo ""
-echo ""
-echo "Inatalling Kanata v${KANATA_VERSION}"
-if [ ! -d "${KANATA_GIT_CHECKOUT}" ]; then
-  git clone "${KANATA_GIT_REPO}" "${KANATA_GIT_CHECKOUT}"
-fi
-cd "${KANATA_GIT_CHECKOUT}" && git checkout "v${KANATA_VERSION}" &>/dev/null && cd ..
-cargo install --path "./${KANATA_GIT_CHECKOUT}/" &>/dev/null
+function install-kanata-binary() {
+  echo ""
+  echo ""
+  echo "Inatalling Kanata v${KANATA_VERSION}"
 
-OS="$(uname)"
+  if [ ! -d "${KANATA_GIT_CHECKOUT}" ]; then
+    git clone "${KANATA_GIT_REPO}" "${KANATA_GIT_CHECKOUT}"
+  fi
+  cd "${KANATA_GIT_CHECKOUT}" && git checkout "v${KANATA_VERSION}" &>/dev/null && cd ..
+  cargo install --path "./${KANATA_GIT_CHECKOUT}/" &>/dev/null
+}
+
+if command -v nixos-rebuild >/dev/null; then
+  OS="NixOS"
+else
+  OS="$(uname)"
+fi
 case $OS in
+  "NixOS")
+    echo "To install Kanata on NixOS:"
+    echo "import './kanata_install_nixos.nix' in configuration.nix"
+    ;;
   "Linux")
+    install-kanata-binary
     bash ./kanata_install_linux.sh
     ;;
   "Darwin")
+    install-kanata-binary
     bash ./kanata_install_macos.sh
     ;;
   *)
