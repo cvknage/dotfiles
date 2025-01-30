93854a82cd0e70b6921fa13457baf8ce417dcb57
Christophe Knage <cvknage@gmail.com>
2025-01-30 22:16:30 +0100

    fix markdown preview build function

--- neovim/logic/lua/plugins/markdown.lua
+++ neovim/logic/lua/plugins/markdown.lua
@@ -3,13 +3,13 @@ return {
     "iamcco/markdown-preview.nvim",
     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
     ft = { "markdown" },
-    build = function() vim.fn["mkdp#util#install"]() end,
+    build = ":call mkdp#util#install()",
   },
   {
     "MeanderingProgrammer/render-markdown.nvim",
     -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
     -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
-    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+    dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
     cmd = { "RenderMarkdown" },
     ft = { "markdown" },
     ---@module 'render-markdown'
@@ -19,12 +19,12 @@ return {
         -- Determines how the top / bottom of code block are rendered:
         --  thick: use the same highlight as the code body
         --  thin:  when lines are empty overlay the above & below icons
-        above = '',
+        above = "",
         -- Used below code blocks for thin border
-        below = '',
+        below = "",
         -- Highlight for code blocks
-        highlight = 'none',
+        highlight = "none",
       },
     },
-  }
+  },
 }
