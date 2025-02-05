fd1db71bbf5dd1a3084ba982f44251745c0de2df
Christophe Knage <cvknage@gmail.com>
2025-02-05 08:26:52 +0100

    added gcc as it is needed by cargo to build rust project

--- rust/default.nix
+++ rust/default.nix
@@ -1,7 +1,15 @@
-{pkgs, ...}: {
-  home.packages = [
-    pkgs.rustup
-    pkgs.cargo-generate
-    pkgs.rustlings
-  ];
+{
+  lib,
+  pkgs,
+  ...
+}: {
+  home.packages =
+    [
+      pkgs.rustup
+      pkgs.cargo-generate
+      pkgs.rustlings
+    ]
+    ++ lib.optionals (!pkgs.stdenv.isDarwin) [
+      pkgs.gcc # rust needs a C compiler
+    ];
 }
