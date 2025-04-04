7862bc5ce440bcf7872a9f93c96dc86ae091c23c
Christophe Knage <cvknage@gmail.com>
2025-04-04 14:48:20 +0200

    updated bindings for copilotchat

--- neovim/logic/lua/plugins/ai/copilot.lua
+++ neovim/logic/lua/plugins/ai/copilot.lua
@@ -65,6 +65,9 @@ return {
   {
     "CopilotC-Nvim/CopilotChat.nvim",
     enabled = utils.has_dotnet,
+    keys = {
+      { "<leader>ac", "<cmd>CopilotChat<cr>", desc = "CopilotChat" },
+    },
     dependencies = {
       { "zbirenbaum/copilot.lua" },
       { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
@@ -72,6 +75,12 @@ return {
     build = "make tiktoken", -- Only on MacOS or Linux
     opts = {
       -- See Configuration section for options
+      mappings = {
+        reset = {
+          normal = "<C-r>",
+          insert = "<C-r>",
+        },
+      },
     },
     -- See Commands section for default commands if you want to lazy load on them
   },
