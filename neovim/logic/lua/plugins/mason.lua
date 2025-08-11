3c80072ac6290230f7c9ae7e0f2f872fd0ca12b2
Christophe Knage <cvknage@gmail.com>
2025-08-11 12:03:36 +0200

    added mason-lock.nvim

--- neovim/logic/lua/plugins/mason.lua
+++ neovim/logic/lua/plugins/mason.lua
@@ -1,23 +1,33 @@
 return {
-  "mason-org/mason.nvim",
-  lazy = true,
-  cmd = {
-    "Mason",
-    "MasonUpdate",
-    "MasonInstall",
-    "MasonUninstall",
-    "MasonUninstallAll",
-    "MasonLog",
+  {
+    "mason-org/mason.nvim",
+    lazy = true,
+    cmd = {
+      "Mason",
+      "MasonUpdate",
+      "MasonInstall",
+      "MasonUninstall",
+      "MasonUninstallAll",
+      "MasonLog",
+    },
+    -- config = true,
+    config = function(_, _)
+      -- Add custom mason registry to allow mason to download roslyn.
+      -- Remove this config section and use "config = true" instead when mason has the roslyn package natievly.
+      require("mason").setup({
+        registries = {
+          "github:mason-org/mason-registry",
+          "github:Crashdummyy/mason-registry",
+        },
+      })
+    end,
   },
-  -- config = true,
-  config = function(_, _)
-    -- Add custom mason registry to allow mason to download roslyn.
-    -- Remove this config section and use "config = true" instead when mason has the roslyn package natievly.
-    require("mason").setup({
-      registries = {
-        "github:mason-org/mason-registry",
-        "github:Crashdummyy/mason-registry",
-      },
-    })
-  end,
+  {
+    "zapling/mason-lock.nvim",
+    init = function()
+      require("mason-lock").setup({
+        lockfile_path = vim.fn.stdpath("config") .. "/mason-lock.json",
+      })
+    end,
+  },
 }
