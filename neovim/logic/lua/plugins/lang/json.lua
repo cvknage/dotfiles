03d73a9d5d4823f795827fa6886572246b24698c
Christophe Knage <cvknage@gmail.com>
2025-09-27 00:11:11 +0200

    simplified yaml and json config, I was just setting the default settings anyways

--- neovim/logic/lua/plugins/lang/json.lua
+++ neovim/logic/lua/plugins/lang/json.lua
@@ -5,17 +5,6 @@ return {
   },
   {
     "neovim/nvim-lspconfig",
-    opts = function(_, opts)
-      table.insert(opts.ensure_installed, "jsonls")
-      vim.lsp.config("jsonls", {
-        settings = {
-          json = {
-            format = { enable = true },
-            validate = { enable = true },
-          },
-        },
-      })
-      return opts
-    end,
+    opts = { ensure_installed = { "jsonls" } },
   },
 }
--- neovim/logic/lua/plugins/lang/yaml.lua
+++ neovim/logic/lua/plugins/lang/yaml.lua
@@ -5,20 +5,6 @@ return {
   },
   {
     "neovim/nvim-lspconfig",
-    opts = function(_, opts)
-      table.insert(opts.ensure_installed, "yamlls")
-      vim.lsp.config("yamlls", {
-        settings = {
-          redhat = { telemetry = { enabled = false } },
-          yaml = {
-            format = {
-              enable = true,
-              -- singleQuote = true,
-            },
-          },
-        },
-      })
-      return opts
-    end,
+    opts = { ensure_installed = { "yamlls" } },
   },
 }
