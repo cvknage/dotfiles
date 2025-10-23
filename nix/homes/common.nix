08075444688aa8911340d03b0de0426cf6faa363
Christophe Knage <cvknage@gmail.com>
2025-10-23 14:13:16 +0200

    reordered some config

--- nix/homes/common.nix
+++ nix/homes/common.nix
@@ -31,22 +31,12 @@
       pkgs.gnumake
     ];
 in {
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
-  home.stateVersion = "24.11"; # Please read the comment before changing.
-
   imports = [
     # shared between all
     ../../rust
   ];
 
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
+  # The home.packages option allows you to install Nix packages into your environment.
   home.packages = [
     # Developer Tools
     pkgs.tmux
@@ -109,6 +99,9 @@ in {
     nix-direnv.enable = true;
   };
 
+  # Enable management of XDG base directories.
+  xdg.enable = true;
+
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
   home.file = {
@@ -144,9 +137,6 @@ in {
     "${config.xdg.configHome}/yazi".source = "${dotfiles}/yazi";
   };
 
-  # Enable management of XDG base directories.
-  xdg.enable = true;
-
   # Home Manager can also manage your environment variables through
   # 'home.sessionVariables'. These will be explicitly sourced when using a
   # shell provided by Home Manager. If you don't want to manage your shell
@@ -165,4 +155,13 @@ in {
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
+
+  # This value determines the Home Manager release that your configuration is
+  # compatible with. This helps avoid breakage when a new Home Manager release
+  # introduces backwards incompatible changes.
+  #
+  # You should not change this value, even if you update Home Manager. If you do
+  # want to update the value, then make sure to first check the Home Manager
+  # release notes.
+  home.stateVersion = "24.11"; # Please read the comment before changing.
 }
