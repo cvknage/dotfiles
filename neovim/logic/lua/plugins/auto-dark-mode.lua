1f2c5f5c01fc70cf773ce2d7aa4b50f6f0b3d295
Christophe Knage <cvknage@gmail.com>
2026-01-15 15:56:54 +0100

    enable vim auto dark mode polling in work config

--- neovim/logic/lua/plugins/auto-dark-mode.lua
+++ neovim/logic/lua/plugins/auto-dark-mode.lua
@@ -1,9 +1,13 @@
+local utils = require("utils")
+
 return {
   "f-person/auto-dark-mode.nvim",
   -- https://github.com/f-person/auto-dark-mode.nvim?tab=readme-ov-file#%EF%B8%8F-configurationk
   opts = {},
   config = function(_, opts)
     require("auto-dark-mode").setup(opts)
-    require("auto-dark-mode").disable() -- Disable polling after initial startup
+    if not utils.is_work_config then
+      require("auto-dark-mode").disable() -- Disable polling after initial startup
+    end
   end,
 }
